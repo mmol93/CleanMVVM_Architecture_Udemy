3c862912a7996977930873c74f590b3f25b8806d
ljws93 <ljws93@naver.com>
2021-07-29 22:09:27 +0900

    basic viewModel
    1. make viewModel class
    
    2. connect view with viewModel
    - set upCount function
    
    3. test
    - rotating test
    - button click listener and check upCount function

--- app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.viewmodel_udemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodel_udemy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binder : ActivityMainBinding
+    private lateinit var viewModel : MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+        // ViewModel 대상 설정
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binder.textView.text = viewModel.getCurrentCount().toString()
+        binder.button.setOnClickListener {
+            binder.textView.text = viewModel.getUpdatedCount().toString()
+        }
     }
 }
--- app/src/main/java/com/example/viewmodel_udemy/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivityViewModel.kt
@@ -3,4 +3,14 @@ package com.example.viewmodel_udemy
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
+    private var count = 0
+
+    fun getCurrentCount() : Int{
+
+        return count
+    }
+
+    fun getUpdatedCount() : Int{
+        return ++count
+    }
 }
