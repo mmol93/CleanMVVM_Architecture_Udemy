f76b4a36d8d0f1827573af0dbe9b56b15f9edef8
ljws93 <ljws93@naver.com>
2021-07-30 20:23:01 +0900

    adapt basic liveData
    1. plus inital data and editText data with LiveData
    
    2. use observe

--- app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.viewmodel_udemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodel_udemy.databinding.ActivityMainBinding
 
@@ -17,11 +18,14 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = ViewModelFactory(125)
         // ViewModel 대상 설정
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
+        // observe를 이용하여 관찰할 대상(변수) 설정
+        viewModel.total.observe(this, Observer {
+            binder.textView.text = it.toString()
+        })
 
-        binder.textView.text = viewModel.getTotal().toString()
+
         binder.addButton.setOnClickListener {
             viewModel.add(binder.editText.text.toString().toInt())
-            binder.textView.text = viewModel.getTotal().toString()
         }
     }
 }
--- app/src/main/java/com/example/viewmodel_udemy/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivityViewModel.kt
@@ -1,20 +1,21 @@
 package com.example.viewmodel_udemy
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal : Int) : ViewModel() {
-    private var total = 0
+    // LiveData를 사용
+    // 혹시 변수에 private를 사용한다면 get()을 써서 이 라이브데이터를 지정해줘야한다
+    var total = MutableLiveData<Int>()
 
     init {
-        total = startingTotal
-    }
-
-    fun getTotal() : Int{
-
-        return total
+        // MutableLiveData의 경우 해당 값을 .value로 지정할 수 있다
+        total.value = startingTotal
     }
 
     fun add(input : Int){
-        total += input
+        // .value의 경우 자동으로 Int가 아니기 때문에 연산을 위해서는
+        // 함수를 사용하거나 .toInt() 같은 것을 활용해야한다
+        total.value = (total.value)?.plus(input)
     }
 }
