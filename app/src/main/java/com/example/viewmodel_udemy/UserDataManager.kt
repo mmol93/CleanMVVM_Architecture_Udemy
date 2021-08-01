98a93edcee232c935d89ab249ee6067d5f0076d1
ljws93 <ljws93@naver.com>
2021-08-01 20:37:43 +0900

    add some comments(not recommended way)

--- app/src/main/java/com/example/viewmodel_udemy/UserDataManager.kt
+++ app/src/main/java/com/example/viewmodel_udemy/UserDataManager.kt
@@ -22,6 +22,7 @@ class UserDataManager {
         // 첫 번째 변수인 count는 기다리지 않기 때문에 초기 값인 0이 바로 들어가고
         // 두 번째 변수인 deferred는 await()가 있기 때문에 해당 변수만 끝날 때까지 기다렸다가 넣어준다
         // 즉, delay에 의해 deferred 값을 얻기 위해 3초를 기다리지만 count는 return에 반영되지 않음
+        // 그래서 이러한 방법은 권장되지 않는 사용법임
         return count + deferred.await()
     }
 }
