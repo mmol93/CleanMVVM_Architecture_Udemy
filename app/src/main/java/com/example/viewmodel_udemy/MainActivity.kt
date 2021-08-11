dd9e30e3a55a9a30d271736e99d3e21754a64495
ljws93 <ljws93@naver.com>
2021-08-11 19:42:59 +0900

    make simple WorkManager(OneTimeWorkRequest)
    1. make Work Class and set doWork method
    
    2. run No.1 with OneTimeWorkRequest

--- app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.viewmodel_udemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.viewmodel_udemy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binder : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binder = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binder.button.setOnClickListener {
+            setOnTimeWorkRequest()
+        }
+    }
+
+    private fun setOnTimeWorkRequest(){
+        // WorkManager의 실행은 WorkManager 서비스로 할 수 있다
+        // OneTimeWorkRequest: 한 번만 실행한다
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        // enqueue를 사용하여 workRequest를 workManager에 제출한다
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
     }
 }
