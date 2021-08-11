0ac566393a1b63a3aaadc1637a98c310533ef244
ljws93 <ljws93@naver.com>
2021-08-11 20:21:11 +0900

    observe WorkManager State
    1. How to observe state of workManager
    - use LiveData & observe
    - display state of workManager by state.name

--- app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewmodel_udemy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.example.viewmodel_udemy.databinding.ActivityMainBinding
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOnTimeWorkRequest(){
+        val workManager = WorkManager.getInstance(applicationContext)
         // WorkManager의 실행은 WorkManager 서비스로 할 수 있다
         // OneTimeWorkRequest: 한 번만 실행한다
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
@@ -26,5 +28,13 @@ class MainActivity : AppCompatActivity() {
         // enqueue를 사용하여 workRequest를 workManager에 제출한다
         WorkManager.getInstance(applicationContext)
             .enqueue(uploadRequest)
+
+        // WorkManager 인스턴스의 id를 이용하여 LiveData로 observe를 할 수 있다
+        workManager.getWorkInfoByIdLiveData(uploadRequest.id)
+                // workManager의 상태를 observe 한다
+            .observe(this, Observer {
+                // state: Blocked -> Enqueued -> Running -> Succeeded
+                binder.textView.text = it.state.name
+            })
     }
 }
