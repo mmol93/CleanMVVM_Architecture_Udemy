cdf80825885f413d7f5333b5cbeee4c7d8958661
ljws93 <ljws93@naver.com>
2021-12-20 20:10:55 +0900

    basic viewModel

--- app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.viewmodel_udemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodel_udemy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.textView.text = viewModel.getCurrentCount().toString()
+
+        binding.button.setOnClickListener {
+            binding.textView.text = viewModel.getUpdatedCount().toString()
+        }
     }
 }
--- app/src/main/java/com/example/viewmodel_udemy/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodel_udemy/MainActivityViewModel.kt
@@ -2,5 +2,13 @@ package com.example.viewmodel_udemy
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel:ViewModel() {
+    private var count = 0
+
+    fun getCurrentCount():Int{
+        return count
+    }
+    fun getUpdatedCount():Int{
+        return ++count
+    }
 }
